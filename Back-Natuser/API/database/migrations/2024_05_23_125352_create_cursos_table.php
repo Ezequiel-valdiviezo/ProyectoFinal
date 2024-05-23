8a7dd0da4b4a8e804bc00309161fe401fddfc18f
Ezequiel-valdiviezo <ezequiel.valdiviezo02@gmail.com>
2024-05-23 10:15:17 -0300

    Rutas, controller, migracion, modelo de cursos

---
+++ Back-Natuser/API/database/migrations/2024_05_23_125352_create_cursos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cursos', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo', 256);
+            // $table->string('imagen', 256)->nullable();
+            $table->string('descripcion_breve', 256);
+            $table->string('categoria', 256);
+            $table->text('descripcion_completa');
+            $table->unsignedInteger('precio');
+            $table->string('telefono', 256);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cursos');
+    }
+};
--- Back-Natuser/API/routes/api.php
+++ Back-Natuser/API/routes/api.php
@@ -3,6 +3,22 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/cursos', function () {
+    return 'Lista de cursos';
+});
+Route::get('/cursos/{id}', function () {
+    return 'Lista de 1 curso';
+});
+Route::post('/cursos', function () {
+    return 'Creando cursos';
+});
+Route::put('/cursos/{id}', function () {
+    return 'Actualizando cursos';
+});
+Route::delete('/cursos/{id}', function () {
+    return 'Borrando cursos';
+});
