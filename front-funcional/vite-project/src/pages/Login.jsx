405ad0f1b722fd16e261dcbb3afe3d62292dcf59
Ezequiel-valdiviezo <ezequiel.valdiviezo02@gmail.com>
2024-05-31 14:31:00 -0300

    Diseño de login y register

--- front-funcional/vite-project/src/pages/Login.jsx
+++ front-funcional/vite-project/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import '../styles/login.css'
+import imgg from '../assets/logo.png'
 
 function Login({ onToggle }){
 
@@ -24,10 +25,12 @@ function Login({ onToggle }){
 
   return(
     <div className="login d-flex flex-column justify-content-center align-items-center text-center vh-100">
-      <div className="cardContenido">
-        <h1>¡Bienvenido a NatUser!</h1>
-
-        <div>
+      <div className="cardContenido p-5">
+          <h1>¡Bienvenido a NatUser!</h1>
+          {/* <img src={imgg} alt="" width="100px"/> */}
+          {/* <p>¡Nos alegra tener aqui!</p> */}
+          <p>Inicia sesión para acceder a contenido exclusivo, participar en foros y conectarte con otros padres que están viviendo la misma experiencia que tú.</p>
+          <h2>Iniciar Sesión</h2>
           <form onSubmit={handleSubmit}>
           <div className="d-flex flex-column m-3">
             <label htmlFor="email">Email:</label>
@@ -52,7 +55,6 @@ function Login({ onToggle }){
           <button className="btn btn-outline-primary" type="submit">Iniciar sesión</button>
           </form>
         <button onClick={onToggle} className="btn btn-outline-primary mt-3">Registro</button>
-        </div>   
       </div>
     </div>
   )
--- front-funcional/vite-project/src/pages/Register.jsx
+++ front-funcional/vite-project/src/pages/Register.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import '../styles/register.css'
+import imgg from '../assets/be.png'
+
 
 function Register({ onToggle }){
 
@@ -25,46 +27,46 @@ function Register({ onToggle }){
 
   return(
     <>
-    <div className="login d-flex flex-column justify-content-center align-items-center text-center vh-100">
-      <div className="cardContenido">
-      <h1>¡Bienvenido a NatUser!</h1>
-      <p>Si eres nuevo crea tu usuario por primera vez</p>
-
-      <form onSubmit={handleSubmit}>
-      <div className="d-flex flex-column m-3">
-        <label htmlFor="name">Nombre:</label>
-        <input
-          type="name"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="d-flex flex-column m-3">
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="d-flex flex-column m-3">
-        <label htmlFor="password">Contraseña:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-      </div>
-      <button className="btn btn-outline-primary" type="submit">Registrarse</button>
-    </form>
-    <button className="btn btn-outline-primary mt-3" onClick={onToggle}>Iniciar sesión</button>
-    </div>
+    <div className="register d-flex flex-column justify-content-center align-items-center text-center vh-100">
+      <div className="cardContenido p-5">
+        <h1>¡Bienvenido a NatUser!</h1>
+            <p>Registrate y accede a todas las funcionalidad para nuestros usuarios.<br /> ¡Te deseamos lo mejor en esta increíble aventura!</p>
+            <h2>Registrarme</h2>
+            <form onSubmit={handleSubmit}>
+            <div className="d-flex flex-column m-3">
+              <label htmlFor="name">Nombre de usuario:</label>
+              <input
+                type="name"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="d-flex flex-column m-3">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="d-flex flex-column m-3">
+              <label htmlFor="password">Contraseña:</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+              />
+            </div>
+            <button className="btn btn-outline-primary" type="submit">Registrarme</button>
+            </form>
+          <button onClick={onToggle} className="btn btn-outline-primary mt-3">Iniciar sesión</button>
+          </div> 
     </div>
 
     </>
