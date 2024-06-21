d527af69108d962b5a3457ae2ea2fb6c37ae8ced
Ezequiel-valdiviezo <ezequiel.valdiviezo02@gmail.com>
2024-06-21 09:49:36 -0300

    Nuevo diseño perfil

--- front-funcional/vite-project/src/pages/Perfil.jsx
+++ front-funcional/vite-project/src/pages/Perfil.jsx
@@ -6,28 +6,74 @@ import img from '../assets/2.png'
 function Perfil(){
 
     return(
-        <div className="perfil mt-5 p-5">
-            <div className="row">
-                <div className="col-12">
-                <h2 className="mb-4 text-center">Mi Perfil</h2>
+        <section className="" style={{ backgroundColor: "#f4f5f7" }}>
+        <div className="container py-5 h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col col-lg-6 mb-4 mb-lg-0">
+              <div className="card mb-3" style={{ borderRadius: ".5rem" }}>
+                <div className="row g-0">
+                  <div
+                    className="col-md-4 gradient-custom text-center text-white"
+                    style={{
+                      borderTopLeftRadius: ".5rem",
+                      borderBottomLeftRadius: ".5rem",
+                    }}
+                  >
+                    <img
+                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+                      alt="Avatar"
+                      className="img-fluid my-5"
+                      style={{ width: "80px" }}
+                    />
+                    <h5>Marie Horwitz</h5>
+                    <p>Web Designer</p>
+                    <i className="far fa-edit mb-5"></i>
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body p-4">
+                      <h6>Information</h6>
+                      <hr className="mt-0 mb-4" />
+                      <div className="row pt-1">
+                        <div className="col-6 mb-3">
+                          <h6>Email</h6>
+                          <p className="text-muted">info@example.com</p>
+                        </div>
+                        <div className="col-6 mb-3">
+                          <h6>Phone</h6>
+                          <p className="text-muted">123 456 789</p>
+                        </div>
+                      </div>
+                      <h6>Projects</h6>
+                      <hr className="mt-0 mb-4" />
+                      <div className="row pt-1">
+                        <div className="col-6 mb-3">
+                          <h6>Recent</h6>
+                          <p className="text-muted">Lorem ipsum</p>
+                        </div>
+                        <div className="col-6 mb-3">
+                          <h6>Most Viewed</h6>
+                          <p className="text-muted">Dolor sit amet</p>
+                        </div>
+                      </div>
+                      <div className="d-flex justify-content-start">
+                        <a href="#!">
+                          <i className="fab fa-facebook-f fa-lg me-3"></i>
+                        </a>
+                        <a href="#!">
+                          <i className="fab fa-twitter fa-lg me-3"></i>
+                        </a>
+                        <a href="#!">
+                          <i className="fab fa-instagram fa-lg"></i>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
                 </div>
+              </div>
             </div>
-            <div className="row align-items-center m-auto">
-                <div className="col-md-4 text-center">
-                <img 
-                    src={img} 
-                    alt="Foto de perfil" 
-                    className="imagenPerfil" 
-                    width="100%"
-                />
-                <button className="mt-2 btn btn-outline-light">Camabiar</button>
-                </div>
-                <div className="col-md-8">
-                <h3>Ezequiel</h3>
-                <p>Ezequiel@gmail.com</p>
-                </div>
-            </div>
+          </div>
         </div>
+      </section>
     )
 }
 
