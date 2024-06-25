92c7f0959e8405075c39f514c632dfd9057705f8
Ezequiel-valdiviezo <ezequiel.valdiviezo02@gmail.com>
2024-06-25 13:16:14 -0300

    Modal cursos

--- front-funcional/vite-project/src/pages/Cursos.jsx
+++ front-funcional/vite-project/src/pages/Cursos.jsx
@@ -1,22 +1,20 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import '../styles/cursos.css';
-import img from '../assets/1.png'
+import img from '../assets/1.png';
 
-function Cursos(){
+function Cursos() {
 
     const [cursos, setCursos] = useState([]);
-    const [mostrarCursos, setMostrarCursos] = useState(false);
     const [cursoSeleccionado, setCursoSeleccionado] = useState(null);
 
     useEffect(() => {
         fetch('http://127.0.0.1:8000/api/cursos', {
             method: 'GET',
             credentials: 'include'
-        }
-        )
-            .then(response => response.json())
-            .then(data => setCursos(data))
-            .catch(error => console.error('Error fetching cursos:', error));
+        })
+        .then(response => response.json())
+        .then(data => setCursos(data))
+        .catch(error => console.error('Error fetching cursos:', error));
     }, []);
 
     const handleMostrarDetalles = (index) => {
@@ -24,52 +22,51 @@ function Cursos(){
     };
 
     const handleCerrarDetalles = () => {
-        setCursoSeleccionado(null)
-    }
+        setCursoSeleccionado(null);
+    };
 
-    return(
+    return (
         <div className="fondoCursos">
-        <div className="cursos text-center">
-        <h2 className="pt-5">Cursos</h2>
-        
+            <div className="cursos text-center">
+                <h2 className="pt-5">Cursos</h2>
+
                 <div className="d-flex flex-wrap justify-content-center">
                     {cursos.map((curso, index) => (
-                        // <div className="CardCurso" key={index}>
-                            <div className="card curso m-2" style={{ width: '18rem' }}>
-                                {/* <img src={img} width="100%" className="card-img-top" alt="" /> */}
-                                <img src={'http://127.0.0.1:8000' + curso.imagen} width="100%" className="card-img-top" alt="" />
-                                
-                                <ul className="list-group list-group-flush">
-                                    <li className="list-group-item">
-                                        <h3 className="card-title">{curso.titulo}</h3>
-                                    </li>
-                                    <li className="list-group-item">Categoria: {curso.categoria}</li>
-                                    <li className="list-group-item">Teléfono: {curso.telefono}</li>
-                                    <li className="list-group-item">Precio: ${curso.precio}</li>
-                                    <li className="list-group-item">
-                                        <button className="btn btn-outline-primary m-1" onClick={() => handleMostrarDetalles(index)}>Ver Detalles</button>
-                                        <button className="btn btn-outline-primary m-1">Continuar</button>
-                                    </li>
-                                </ul>
-                            </div>
-                        // </div>
+                        <div className="card curso m-2" style={{ width: '18rem' }} key={index}>
+                            <img src={'http://127.0.0.1:8000' + curso.imagen} width="100%" className="card-img-top" alt="" />
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item">
+                                    <h3 className="card-title">{curso.titulo}</h3>
+                                </li>
+                                <li className="list-group-item">Categoria: {curso.categoria}</li>
+                                <li className="list-group-item">Teléfono: {curso.telefono}</li>
+                                <li className="list-group-item">Precio: ${curso.precio}</li>
+                                <li className="list-group-item">
+                                    <button className="btn btn-outline-primary m-1" onClick={() => handleMostrarDetalles(index)}>Ver Detalles</button>
+                                    <button className="btn btn-outline-primary m-1">Continuar</button>
+                                </li>
+                            </ul>
+                        </div>
                     ))}
                 </div>
 
-            {cursoSeleccionado && (
-                <div className="detalles-curso">
-                    <h3>Detalles del Curso</h3>
-                    <p>Título: {cursoSeleccionado.titulo}</p>
-                    <p>Descripción: {cursoSeleccionado.descripcion_completa}</p>
-                    <p>Categoría: {cursoSeleccionado.categoria}</p>
-                    <p>Teléfono: {cursoSeleccionado.telefono}</p>
-                    <p>Precio: {cursoSeleccionado.precio}</p>
-                    <button onClick={handleCerrarDetalles}>Cerrar</button>
-                </div>
-            )} 
+                {cursoSeleccionado && (
+                    <div className="modal">
+                        <div className="modal-content">
+                            <span className="modal-close" onClick={handleCerrarDetalles}>&times;</span>
+                            <h3>Detalles del Curso</h3>
+                            <p>Título: {cursoSeleccionado.titulo}</p>
+                            <p>Descripción: {cursoSeleccionado.descripcion_completa}</p>
+                            <p>Categoría: {cursoSeleccionado.categoria}</p>
+                            <p>Teléfono: {cursoSeleccionado.telefono}</p>
+                            <p>Precio: ${cursoSeleccionado.precio}</p>
+                            <button onClick={handleCerrarDetalles}>Cerrar</button>
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
-        </div>
-    )
+    );
 }
 
 export default Cursos;
