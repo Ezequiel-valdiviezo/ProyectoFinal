4eb46e027168033adca482bfe37a4a924977e739
Ezequiel-valdiviezo <ezequiel.valdiviezo02@gmail.com>
2024-07-08 15:03:37 -0300

    Borrar cursos

--- front-funcional/vite-project/src/pages/admin/ManejoCursos.jsx
+++ front-funcional/vite-project/src/pages/admin/ManejoCursos.jsx
@@ -3,7 +3,20 @@ import { useNavigate } from "react-router-dom";
 import '../../styles/adminManejoCursos.css'
 
 function ManejoCursos(){
+    const [cursos, setCursos] = useState([]);
+
     const navigate = useNavigate();
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/api/cursos', {
+            method: 'GET',
+            credentials: "include",
+        })
+        .then(response => response.json())
+        .then(data => setCursos(data))
+        .catch(error => console.error('Error fetch cursos:', error));
+    }, []);
+
     useEffect(() => {
         const usuario = JSON.parse(localStorage.getItem('user'));
         if(usuario.user.role == "admin"){
@@ -38,6 +51,23 @@ function ManejoCursos(){
           });
         }
       };
+
+      const handleDelete = async (id) => {
+        try {
+            const response = await fetch(`http://127.0.0.1:8000/api/cursos/${id}`, {
+                method: 'DELETE',
+                credentials: 'include'
+            });
+            if(response.ok){
+                console.log("Curso eliminado correctamente");
+                setCursos(cursos.filter(curso => curso.id !== id));
+            }else {
+                console.error("Error al eliminar el curso")
+            }
+        } catch (error) {
+            console.log("Error en la solicitus de eliminació", error);
+        }
+      }
     
       const handleSubmit = async (e) => {
         e.preventDefault();
@@ -73,7 +103,7 @@ function ManejoCursos(){
     return(
         <>
         <div className="vh-100">
-                <div className="adminManejoCursos pt-5 text-center">
+                <div className="adminManejoCursos pt-5 pb-5 text-center">
 
                     <div className="saludo">
                         <h2>Crear cursos</h2>
@@ -87,9 +117,9 @@ function ManejoCursos(){
                         </div>
                         <div className="form-group my-4">
                         <label htmlFor="imagen">Imagen</label>
-                        <inputd
+                        <input
                             type="file"
-                            className="form-control p-1 mt-2"
+                            className="form-control mt-2"
                             id="imagen"
                             name="imagen"
                             accept="image/*"
@@ -118,6 +148,36 @@ function ManejoCursos(){
                         </div>
                         <button type="submit" className="btn btn-outline-primary">Crear Curso</button>
                         </form>
+
+                {Array.isArray(cursos) && cursos.length > 0 ? (
+                    <table className="table mt-5 table-striped table-hover">
+                        <thead className="table-dark">
+                            <tr>
+                                <th scope="col">Nombre</th>
+                                <th scope="col">Descripción</th>
+                                <th scope="col">Teléfono</th>
+                                <th scope="col">Precio</th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {cursos.map((curso, index) => (
+                                <tr key={index}>
+                                    <td>{curso.titulo}</td>
+                                    <td>{curso.descripcion_breve}</td>
+                                    <td>{curso.telefono}</td>
+                                    <td>${curso.precio}</td>
+                                    <td>
+                                        <button className="btn btn-outline-primary m-1">Detalles</button>
+                                        <button onClick={() => handleDelete(curso.id)} className="btn btn-outline-danger m-1">Eliminar</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                ) : (
+                    <p>No se encontraron cursos.</p>
+                )}
 
                 </div>
             </div>
