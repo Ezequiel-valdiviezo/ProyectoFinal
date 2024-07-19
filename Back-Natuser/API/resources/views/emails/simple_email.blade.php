5d9e4f49893bb3c914b307c48bfc771d22674f80
Ezequiel-valdiviezo <ezequiel.valdiviezo02@gmail.com>
2024-07-19 17:32:02 -0300

    Mejora de diseño del email

--- Back-Natuser/API/resources/views/emails/simple_email.blade.php
+++ Back-Natuser/API/resources/views/emails/simple_email.blade.php
@@ -7,26 +7,29 @@
             font-family: Arial, sans-serif;
             margin: 0;
             padding: 0;
-            background-color: #f4f4f4;
+            background-color: #056cbb;
         }
         .container {
-            width: 100%;
+            max-width: 600px;
             margin: 0 auto;
             padding: 20px;
             background-color: #ffffff;
         }
         .header {
-            background-color: #6c9fcf;
+            background-color: #e0eaf3;
             color: white;
             padding: 10px 0;
             text-align: center;
         }
         .content {
             padding: 20px;
-            text-align: center;
+            text-align: left;
         }
-        .content img {
-            max-width: 100%;
+        .content ul li{
+            list-style: none;
+        }
+        .header img {
+            max-width: 85%;
             height: auto;
         }
         .footer {
@@ -40,11 +43,19 @@
 <body>
     <div class="container">
         <div class="header">
-            <h1>NatUser</h1>
+            {{-- <h1>NatUser</h1> --}}
+            <img src="cid:imagen.jpg" alt="Imagen">
         </div>
         <div class="content">
-            <img src="cid:imagen.jpg" alt="Imagen">
-            <p>Hola de nuevo! Fuiste aceptado para ofrecer tus servicios en nuestra web.</p>
+            <p>¡Felicitaciones! Fuiste aceptado para ofrecer tus servicios en nuestra web.</p>
+            <p>Los planes a seguir son:
+                <ul>
+                    <li>1: Seleccionar su plan</li>
+                    <li>2: Realizar el pago</li>
+                    <li>3: Enviar el comprobante por este medio</li>
+                    <li>4: Esperar hasta 48hs hábiles, en  ese plazo estaremos subiendo su servicio a nuestra web. En caso que pasadas las 48hs no haya sucedido, por favor comunicarse de vuelta. Muchas gracias</li>
+                </ul>
+            </p>
         </div>
         <div class="footer">
             <p>&copy; 2024 NatUser. Todos los derechos reservados.</p>
