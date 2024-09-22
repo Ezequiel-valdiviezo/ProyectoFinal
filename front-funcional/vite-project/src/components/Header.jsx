b858698c95d7bdca5b8f0e8487eea48894f60138
Ezequiel-valdiviezo <ezequiel.valdiviezo02@gmail.com>
2024-09-22 15:56:34 -0300

    Mejora de diseño de nav responsive

--- front-funcional/vite-project/src/components/Header.jsx
+++ front-funcional/vite-project/src/components/Header.jsx
@@ -44,7 +44,7 @@ function Header() {
     
       <nav className="navbar navbar-expand-sm navbar-light ">
         <div className="container-fluid d-flex justify-content-between align-items-center anchoNav">
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler bg-white m-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
